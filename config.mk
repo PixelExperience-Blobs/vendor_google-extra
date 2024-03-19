c232bf6cdc8efe8078ca8ff3a6ec598dbcfd7e23
jro1979oliver <jroliveira.oliveira301@gmail.com>
2024-03-19 18:27:18 +0100

    start the google extra
    Change-Id: Id15f1ec37b74364b1d03780b49e3cb3e6f5d9ad5

--- config.mk
+++ config.mk
@@ -4,5 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/motorola-extra/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/motorola-extra/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/google-extra/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/google-extra/overlay
+
+TARGET_SYSTEM_PROP += vendor/google-extra/system.prop
